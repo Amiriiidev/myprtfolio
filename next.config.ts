a260d0aa63af1764cf8ffd5c397cbe812107aa86
Amiriiidev <hosein.v378@gmail.com>
2025-09-19 22:14:08 +0330

    commited well done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
-module.exports = {
-  basePath: "/myprtfolio",
+/**
+ * @type {import('next').NextConfig}
+ */ module.exports = {
+  basePath: "/docs",
 };
-const nextConfig: NextConfig = {};
+const nextConfig = {
+  output: "export",
 
-export default nextConfig;
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
+
+module.exports = nextConfig;
