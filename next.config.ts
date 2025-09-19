a0735bdf5faa6b1e59ddc2bc1dd034f93aa21d07
Amiriiidev <hosein.v378@gmail.com>
2025-09-19 21:01:24 +0330

    change base name of project

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig = {
-  output: "export", // مهم: خروجی استاتیک
-  basePath: isProd ? "/myprtfolio" : "",
-  assetPrefix: isProd ? "/myprtfolio/" : "",
+import type { NextConfig } from "next";
+module.exports = {
+  basePath: "/myprtfolio",
+};
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: "/myprtfolio",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
